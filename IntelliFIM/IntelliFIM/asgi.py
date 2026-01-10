8a830268a25036af68ab2b04073703446e18010d
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2026-01-10 18:42:22 +0530

    remove the websocket.

--- IntelliFIM/IntelliFIM/asgi.py
+++ IntelliFIM/IntelliFIM/asgi.py
@@ -1,21 +1,7 @@
 # your_project_name/asgi.py
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import path
-import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IntelliFIM.settings')
-django.setup()
 
-from fim.consumers import FIMEventConsumer
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/fim/stream/", FIMEventConsumer.as_asgi()),
-        ])
-    )
-})
+application = get_asgi_application()
