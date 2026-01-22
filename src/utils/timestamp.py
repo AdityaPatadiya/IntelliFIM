b8bcdbb4e13200f14917840b8bc5dd7b62b844a6
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2026-01-22 19:48:18 +0530

    remove old project files.

--- src/utils/timestamp.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import pytz
-
-def timezone():
-    timezone = pytz.timezone("Asia/Kolkata")
-    timestamp = datetime.now(timezone).strftime(r"%Y-%m-%d %H:%M:%S")
-    return timestamp, timezone
